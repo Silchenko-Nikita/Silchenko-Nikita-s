51ac8a99be7a622c7a719ddfb03c2aa9e926d2a1
Silchenko-Nikita <nikita.silchenko.kpi.kp52@gmail.com>
2015-10-11 23:24:52 +0300

    ds

--- courses/prog_base/tasks/long2double/repr.c
+++ courses/prog_base/tasks/long2double/repr.c
@@ -1,12 +1,11 @@
 #include <stdlib.h>
 #include <math.h>
-
 double long2double(long long a){
     char bin[64];
     int i, E = 0;
     double result = 0;
     if (a < 0){
-        unsigned long long a1 = a + (long long) powl(2, 63);
+        unsigned long long a1 = a + 9223372036854775808LL;
         bin[0] = '1';
         for (i = 63; i > 0; i--){
             bin[i] = a1 % 2 ? '1' : '0';
@@ -24,10 +23,7 @@ double long2double(long long a){
         E += bin[11 - i] == '1' ? powl(2, i) : 0 ;
     }
 
-    for (i = 0; i < 64; i++){
-            printf("%c\n", bin[i]);
-        }
-
+    int dd = 1;
     if (E == 0){
         for (i = 12; i < 64; i++){
            result += bin[i] == '1' ? pow(2, (-i + 11) - 1022) : 0;
@@ -36,10 +32,12 @@ double long2double(long long a){
         for (i = 12; i < 64; i++){
            if (bin[i] == '1'){
                 result = NAN;
-                goto point;
+                dd = 0;
            }
         }
-        result = INFINITY;
+        if(dd){
+            result = INFINITY;
+        }
     }else{
         for (i = 12; i < 64; i++){
            result += bin[i] == '1' ? pow(2, (-i + 11) + (E - 1023)) : 0;
@@ -47,7 +45,7 @@ double long2double(long long a){
         result += pow(2, (E - 1023));
     }
 
-    point:
     if (bin[0] == '1') result = -result;
     return result;
 }
+
